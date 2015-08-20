7d0e67000ad8f3b43a37e7605db95ce0403a538e
bko <bko@localhost.localdomain>
2015-08-20 11:29:15 -0400

    MapEditor Fixed globals class, is now a true singleton

--- MapEditor/globals.py
+++ MapEditor/globals.py
@@ -8,25 +8,35 @@ class E_VARS(object):
     def __new__(cls):
         if E_VARS.__instance is None:
             E_VARS.__instance = object.__new__(cls)
+            E_VARS.__instance.__FPS=60 # frames per second
+            E_VARS.__instance.__CELLWIDTH=128
+            E_VARS.__instance.__CELLHEIGHT=64
+            E_VARS.__instance.__CELLRISE=49
+            E_VARS.__instance.__MAPWIDTH=15
+            E_VARS.__instance.__MAPHEIGHT=15
+            E_VARS.__instance.__OLDHEIGHT=pygame.display.Info().current_h
+            E_VARS.__instance.__OLDWIDTH=pygame.display.Info().current_w
+            #assert W_WIDTH%CELLWIDTH==0, "Window width must be a multiple of cell size."
+            #assert W_HEIGHT%CELLHEIGHT==0, "Window height must be a multiple of cell size."
+            E_VARS.__instance.__PATH = 'isoTiles/'
         return E_VARS.__instance
-
-    def __init__(self):
-        # environment vars
-        self.__FPS=60 # frames per second
-        self.__CELLWIDTH=128
-        self.__CELLHEIGHT=64
-        self.__CELLRISE=49
-        self.__MAPWIDTH=15
-        self.__MAPHEIGHT=15
-
-        #assert W_WIDTH%CELLWIDTH==0, "Window width must be a multiple of cell size."
-        #assert W_HEIGHT%CELLHEIGHT==0, "Window height must be a multiple of cell size."
-    
-        self.__PATH = 'isoTiles/'
 
     @property
     def FPS(self):
         return self.__FPS
+
+    @property
+    def OLDHEIGHT(self):
+        return self.__OLDHEIGHT
+    @OLDHEIGHT.setter
+    def OLDHEIGHT(self,val):
+        self.__OLDHEIGHT=val
+    @property
+    def OLDWIDTH(self):
+        return self.__OLDWIDTH
+    @OLDWIDTH.setter
+    def OLDWIDTH(self,val):
+        self.__OLDWIDTH=val
     @property
     def W_WIDTH(self):
         return pygame.display.Info().current_w
